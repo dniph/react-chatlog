c6b4cd664e130994c08ec072f3c981f61d708ef6
Dennif <dennif.pa@gmail.com>
2025-06-11 21:52:51 -0700

    wave 01 implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import React from "react";
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import chat from './data/messages.json';
+
 
 const App = () => {
   return (
@@ -7,8 +11,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry sender = {chat[0].sender} body= {chat[0].body} timeStamp= {chat[0].timeStamp} ></ChatEntry>
       </main>
     </div>
   );
